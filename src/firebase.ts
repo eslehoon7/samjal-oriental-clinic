6a0868c2231ff3731a7730dfada67b626f8285a4
eslehoon7 <eslehoon7@gmail.com>
2026-06-07 22:30:02 +0900

    firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore, doc, getDocFromServer } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: (import.meta as any).env.VITE_FIREBASE_API_KEY || "",
-  authDomain: (import.meta as any).env.VITE_FIREBASE_AUTH_DOMAIN || "",
-  projectId: (import.meta as any).env.VITE_FIREBASE_PROJECT_ID || "",
-  storageBucket: (import.meta as any).env.VITE_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: (import.meta as any).env.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: (import.meta as any).env.VITE_FIREBASE_APP_ID || "",
+  apiKey: "AIzaSyDGQon6PyEj5Co6iucmJRcN5TsWsNJs1TA",
+  authDomain: "samjal-oriental-clinic.firebaseapp.com",
+  projectId: "samjal-oriental-clinic",
+  storageBucket: "samjal-oriental-clinic.firebasestorage.app",
+  messagingSenderId: "440982384374",
+  appId: "1:440982384374:web:66a057b583b0046655c6d7",
 };
 
 // Initialize Firebase
